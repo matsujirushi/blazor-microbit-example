924bfa0c960cc399eeb277294434cab6ae1c0769
Takashi Matsuoka <matsujirushi@live.jp>
2020-12-27 09:57:11 +0900

    Add AccelTable component

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazm.Bluetooth;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace blazor_microbit_example
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazmBluetooth();
 
             await builder.Build().RunAsync();
         }
